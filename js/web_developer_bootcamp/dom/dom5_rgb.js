ba7ebf448ac4d0b90ae38d11f3da718476c8cde4
Aimin Tang <aimin.tang@gmail.com>
2017-12-20 22:07:13 -0600

    dom5 pt4: can test result

--- js/web_developer_bootcamp/dom/dom5_rgb.js
+++ js/web_developer_bootcamp/dom/dom5_rgb.js
@@ -1,14 +1,54 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll('.square');
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
+
+colorDisplay.textContent = pickedColor;
 
 for (var i=0; i<squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
+    squares[i].addEventListener("click", function () {
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = 'Correct!';
+            changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
+        } else {
+            this.style.backgroundColor = '#232323';
+            messageDisplay.textContent = 'Try Again!';
+        }
+    })
+}
+
+function changeColors(color) {
+    for (var i=0; i<squares.length;i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function randomNum(upperLimit) {
+    return Math.floor(Math.random() * upperLimit);
+}
+
+function pickColor() {
+    var r_num = randomNum(colors.length);
+    return colors[r_num];
+}
+
+function generateRandomColors(num) {
+    var colors = [];
+    for (var i=0; i<num; i++) {
+        str = "rgb(";
+        str += randomNum(256);
+        str += ", ";
+        str += randomNum(256);
+        str += ", ";
+        str += randomNum(256);
+        str += ")";
+        colors.push(str);
+    }
+    return colors;
 }
