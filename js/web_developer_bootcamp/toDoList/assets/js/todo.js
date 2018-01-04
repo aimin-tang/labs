ae6b9dc6c1aa597b4c64bff33586c034dc2b5916
Aimin Tang <aimin.tang@gmail.com>
2018-01-04 17:53:08 -0600

    delete new items

--- js/web_developer_bootcamp/toDoList/assets/js/todo.js
+++ js/web_developer_bootcamp/toDoList/assets/js/todo.js
@@ -4,3 +4,18 @@ $("h1").css("color", "purple");
 $("li").click(function () {
     $(this).toggleClass('completed');
 });
+
+$("span").click(function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+      var newToDoItem = $(this).val();
+      $(this).val("");
+      $("ul").append("<li><span>X</span> " + newToDoItem + "</li>");
+  }
+});
