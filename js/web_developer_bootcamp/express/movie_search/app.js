46d9b232ed1a1bbd0d9f83fe3a3a651ff3c87495
Aimin Tang <aimin.tang@gmail.com>
2018-01-16 17:50:05 -0600

    movie search dynamic state

--- js/web_developer_bootcamp/express/movie_search/app.js
+++ js/web_developer_bootcamp/express/movie_search/app.js
@@ -3,9 +3,15 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+    res.render("search");
+})
+
 app.get("/results", function (req, res) {
-    result = request("http://omdbapi.com/?s=california&apikey=thewdb",
-        function (error, response, body) {
+    var search = req.query.search;
+    var url = "http://omdbapi.com/?s=" + search + "&apikey=thewdb";
+
+    request(url, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var data = JSON.parse(body);
                 res.render("results", {data: data});
