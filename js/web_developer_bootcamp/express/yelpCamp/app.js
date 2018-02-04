c162a340e621a42da9bbc2f14aff8c04db0ed520
Aimin Tang <aimin.tang@gmail.com>
2018-02-04 07:21:13 -0600

    comments routes

--- js/web_developer_bootcamp/express/yelpCamp/app.js
+++ js/web_developer_bootcamp/express/yelpCamp/app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
+var Comment = require("./models/comment");
 var seedDB = require("./seeds2");
 
 seedDB();
@@ -49,13 +50,13 @@ app.get("/campgrounds", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("index", {campgrounds: campgrounds});
+            res.render("campgrounds/index", {campgrounds: campgrounds});
         }
     });
 });
 
 app.get("/campgrounds/new", function (req, res) {
-    res.render("new.ejs");
+    res.render("campgrounds/new.ejs");
 });
 
 app.post("/campgrounds", function (req, res) {
@@ -69,7 +70,7 @@ app.post("/campgrounds", function (req, res) {
             console.log(err);
         } else {
             // redirect back to campgrounds
-            res.redirect("/campgrounds");
+            res.redirect("campgrounds/campgrounds");
         }
     });
 });
@@ -79,9 +80,40 @@ app.get("/campgrounds/:id", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
+});
+
+// comments routes
+// ==================
+app.get("/campgrounds/:id/comments/new", function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+        } else{
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            })
+        }
+    })
 });
 
 app.listen(3000, 'localhost', function () {
